e8edc77a3bf3de4a8366e45da2c6f5329ab30998
Ashvinrockz <88977094+ashvin-a@users.noreply.github.com>
2022-10-25 23:06:45 +0530

    Added an index.html

--- Dating-App/core/views.py
+++ Dating-App/core/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>This is the first heading</h1>')
+    return HttpResponse(
+                       '<h1>Wow Python web development is cool</h1>'
+                       '<p> This is a test</p>')
